afdf72012ee5385371b8db7803fe73335203f1ac
Denis <Denis@Laptop>
2013-02-19 14:52:59 +0100

    login 2.0

--- MMS/src/de/team55/mms/db/sql.java
+++ MMS/src/de/team55/mms/db/sql.java
@@ -28,7 +28,7 @@ public class sql {
 	private String dbUser = "root";
 
 	// Datenbankpasswort
-	private String dbPassword = "qwert";
+	private String dbPassword = "";
 
 	public void connect() {
 		try {
