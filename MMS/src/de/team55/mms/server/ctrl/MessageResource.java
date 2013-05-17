5aa4d0387c6f0400057bd595354948081464c694
dw18 <dw18@PC08>
2013-05-17 14:01:10 +0200

    Server kann Objekte Empfangen bis jetzt nur User

--- MMS/src/de/team55/mms/server/ctrl/MessageResource.java
+++ MMS/src/de/team55/mms/server/ctrl/MessageResource.java
@@ -2,7 +2,10 @@ package de.team55.mms.server.ctrl;
 
 import java.util.LinkedList;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,7 +18,25 @@ import de.team55.mms.server.function.User;
 public class MessageResource {
 
 	/**
-	 * returns a User 
+	 * returns a User
+	 * 
+	 * @param user
+	 *            e-Mail of the User
+	 * 
+	 * @return Data of User
+	 */
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	@Path("user/{user}")
+	public User getUser(@PathParam("user") String user) {
+		User tmp = new sql().getUser(user);
+		if (tmp == null)
+			tmp = new User();
+		return tmp;
+	}
+
+	/**
+	 * returns a User
 	 * 
 	 * @param user
 	 *            e-Mail of the User
@@ -25,13 +46,16 @@ public class MessageResource {
 	 */
 	@GET
 	@Produces(MediaType.TEXT_XML)
-	@Path("user/{user}/{pass}")
-	public User getUser(@PathParam("user") String user,
+	@Path("login/{user}/{pass}")
+	public User userLogin(@PathParam("user") String user,
 			@PathParam("pass") String pass) {
-		User tmp = new sql().getUser(user, pass);
+		User tmp = new sql().getUser(user);
 		if (tmp == null)
-			tmp = new User();
-		return tmp;
+			return new User();
+		if (tmp.getPassword().equals(pass))
+			return tmp;
+		else
+			return new User();
 	}
 
 	/**
@@ -45,5 +69,20 @@ public class MessageResource {
 	@Path("users")
 	public LinkedList<User> getAllUsers() {
 		return new sql().userload();
+	}
+
+	@PUT
+	@Path("test/{user}")
+	@Consumes(MediaType.TEXT_XML)
+	@Produces(MediaType.TEXT_XML)
+	public String postMessage(@PathParam("user") User user) {
+		return String.format(user.getNachname());
+	}
+
+	@POST
+	@Path("test/post/")
+	@Consumes(MediaType.TEXT_XML)
+	public String userOut(User user) {
+		return user.getNachname();
 	}
 }
